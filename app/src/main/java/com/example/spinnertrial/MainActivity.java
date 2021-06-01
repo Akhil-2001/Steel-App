fea8337945b9d9e49728926ada41d799c5b90f90
Aniruddh Acharya <aniruddh.acharya3005@gmail.com>
2021-06-01 18:48:16 +0530

    light

--- app/src/main/java/com/example/spinnertrial/MainActivity.java
+++ app/src/main/java/com/example/spinnertrial/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.spinnertrial;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -23,8 +25,145 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     Button reset;
     int total[] = new int[3];
 
-    int count=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
+        spin = (Spinner) findViewById(R.id.dropdown);
+
+        h1 = (TextView) findViewById((R.id.h1));
+        h2 = (TextView) findViewById((R.id.h2));
+        h3 = (TextView) findViewById((R.id.h3));
+        t2 = (TextView) findViewById((R.id.t2));
+        t3 = (TextView) findViewById((R.id.t3));
+        t4 = (TextView) findViewById((R.id.t4));
+        b1 = (EditText) findViewById((R.id.b1));
+        b2 = (EditText) findViewById((R.id.b2));
+        b3 = (EditText) findViewById((R.id.b3));
+        b4 = (EditText) findViewById((R.id.b4));
+        b5 = (EditText) findViewById((R.id.b5));
+        c1 = (TextView) findViewById((R.id.c1));
+        c2 = (TextView) findViewById((R.id.c2));
+        c3 = (TextView) findViewById((R.id.c3));
+        c4 = (TextView) findViewById((R.id.c4));
+        c5 = (TextView) findViewById((R.id.c5));
+        d1 = (TextView) findViewById((R.id.d1));
+        d2 = (TextView) findViewById((R.id.d2));
+        d3 = (TextView) findViewById((R.id.d3));
+        d4 = (TextView) findViewById((R.id.d4));
+        d5 = (TextView) findViewById((R.id.d5));
+
+        reset = (Button) findViewById(R.id.reset);
+
+        spin.setOnItemSelectedListener(this);
+        //Creating the ArrayAdapter instance having the country list
+        ArrayAdapter aa = new ArrayAdapter(this, android.R.layout.simple_spinner_item, units);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Setting the ArrayAdapter data on the Spinner
+        spin.setAdapter(aa);
+
+        TextWatcher textwatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                if(choice.equals("Kgs")){
+                    kgs();
+
+                }
+
+                if(choice.equals("Bundles")){
+                    bundles();
+
+                }
+
+                if(choice.equals("Rods")){
+                    rods();
+
+                }
+                totalcalc();
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        };
+        b1.addTextChangedListener(textwatcher);
+        b2.addTextChangedListener(textwatcher);
+        b3.addTextChangedListener(textwatcher);
+        b4.addTextChangedListener(textwatcher);
+        b5.addTextChangedListener(textwatcher);
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Reset", Toast.LENGTH_LONG).show();
+                b1.setText("");
+                b2.setText("");
+                b3.setText("");
+                b4.setText("");
+                b5.setText("");
+                t2.setText("");
+                t3.setText("");
+                t4.setText("");
+            }
+        });
+
+    }
+
+    //Performing action onItemSelected and onNothing selected
+    @Override
+    public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id) {
+        Toast.makeText(getApplicationContext(), units[position], Toast.LENGTH_LONG).show();
+        choice = spin.getSelectedItem().toString();
+
+        b1.setText("");
+        b2.setText("");
+        b3.setText("");
+        b4.setText("");
+        b5.setText("");
+        t2.setText("");
+        t3.setText("");
+        t4.setText("");
+
+        if (choice.equals("Kgs")) {
+            h1.setText(choice);
+            h2.setText("Bundles");
+            h3.setText("Rods");
+        }
+        if (choice.equals("Rods")) {
+            h1.setText(choice);
+            h2.setText("Bundles");
+            h3.setText("Kgs");
+
+        }
+        if (choice.equals("Bundles")) {
+            h1.setText(choice);
+            h2.setText("Kgs");
+            h3.setText("Rods");
+
+
+        }
+
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void onContentChanged() {
+        super.onContentChanged();
+    }
 
 
     void bundles() {
@@ -42,8 +181,8 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             d1.setText("");
         }
         if (!b2.getText().toString().equals("")) {
-             n[0] = Integer.parseInt(b2.getText().toString())*53;
-             n[1] = Integer.parseInt(b2.getText().toString())*7;
+            n[0] = Integer.parseInt(b2.getText().toString())*53;
+            n[1] = Integer.parseInt(b2.getText().toString())*7;
             c2.setText(String.valueOf(n[0]));
             d2.setText(String.valueOf(n[1]));
         }
@@ -53,10 +192,10 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             d2.setText("");
         }
         if (!b3.getText().toString().equals("")) {
-             n[0] = Integer.parseInt(b3.getText().toString())*53;
-             n[1] = Integer.parseInt(b3.getText().toString())*5;
-             c3.setText(String.valueOf(n[0]));
-             d3.setText(String.valueOf(n[1]));
+            n[0] = Integer.parseInt(b3.getText().toString())*53;
+            n[1] = Integer.parseInt(b3.getText().toString())*5;
+            c3.setText(String.valueOf(n[0]));
+            d3.setText(String.valueOf(n[1]));
         }
         else
         {
@@ -103,8 +242,8 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             d1.setText("");
         }
         if (!b2.getText().toString().equals("")) {
-             n[0] = (int) Math.ceil(Double.parseDouble(b2.getText().toString())/53);
-             n[1] = (int) Math.ceil((Double.parseDouble(b2.getText().toString())*7)/53);
+            n[0] = (int) Math.ceil(Double.parseDouble(b2.getText().toString())/53);
+            n[1] = (int) Math.ceil((Double.parseDouble(b2.getText().toString())*7)/53);
             c2.setText(String.valueOf(n[0]));
             d2.setText(String.valueOf(n[1]));
         }
@@ -114,10 +253,10 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             d2.setText("");
         }
         if (!b3.getText().toString().equals("")) {
-             n[0] = (int) Math.ceil(Double.parseDouble(b3.getText().toString())/53);
-             n[1] = (int) Math.ceil((Double.parseDouble(b3.getText().toString())*5)/53);
-             c3.setText(String.valueOf(n[0]));
-             d3.setText(String.valueOf(n[1]));
+            n[0] = (int) Math.ceil(Double.parseDouble(b3.getText().toString())/53);
+            n[1] = (int) Math.ceil((Double.parseDouble(b3.getText().toString())*5)/53);
+            c3.setText(String.valueOf(n[0]));
+            d3.setText(String.valueOf(n[1]));
         }
         else
         {
@@ -267,146 +406,6 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         t4.setText(String.valueOf(total[2]));
 
     }
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        spin = (Spinner) findViewById(R.id.dropdown);
-
-        h1 = (TextView) findViewById((R.id.h1));
-        h2 = (TextView) findViewById((R.id.h2));
-        h3 = (TextView) findViewById((R.id.h3));
-        t2 = (TextView) findViewById((R.id.t2));
-        t3 = (TextView) findViewById((R.id.t3));
-        t4 = (TextView) findViewById((R.id.t4));
-        b1 = (EditText) findViewById((R.id.b1));
-        b2 = (EditText) findViewById((R.id.b2));
-        b3 = (EditText) findViewById((R.id.b3));
-        b4 = (EditText) findViewById((R.id.b4));
-        b5 = (EditText) findViewById((R.id.b5));
-        c1 = (TextView) findViewById((R.id.c1));
-        c2 = (TextView) findViewById((R.id.c2));
-        c3 = (TextView) findViewById((R.id.c3));
-        c4 = (TextView) findViewById((R.id.c4));
-        c5 = (TextView) findViewById((R.id.c5));
-        d1 = (TextView) findViewById((R.id.d1));
-        d2 = (TextView) findViewById((R.id.d2));
-        d3 = (TextView) findViewById((R.id.d3));
-        d4 = (TextView) findViewById((R.id.d4));
-        d5 = (TextView) findViewById((R.id.d5));
-
-        reset = (Button) findViewById(R.id.reset);
-
-        spin.setOnItemSelectedListener(this);
-        //Creating the ArrayAdapter instance having the country list
-        ArrayAdapter aa = new ArrayAdapter(this, android.R.layout.simple_spinner_item, units);
-        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        //Setting the ArrayAdapter data on the Spinner
-        spin.setAdapter(aa);
-
-        TextWatcher textwatcher = new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-                if(choice.equals("Kgs")){
-                    kgs();
-
-                }
-
-                if(choice.equals("Bundles")){
-                    bundles();
-
-                }
-
-                if(choice.equals("Rods")){
-                    rods();
-
-                }
-                totalcalc();
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        };
-        b1.addTextChangedListener(textwatcher);
-        b2.addTextChangedListener(textwatcher);
-        b3.addTextChangedListener(textwatcher);
-        b4.addTextChangedListener(textwatcher);
-        b5.addTextChangedListener(textwatcher);
-
-        reset.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Reset", Toast.LENGTH_LONG).show();
-                b1.setText("");
-                b2.setText("");
-                b3.setText("");
-                b4.setText("");
-                b5.setText("");
-                t2.setText("");
-                t3.setText("");
-                t4.setText("");
-            }
-        });
-
-    }
-
-    //Performing action onItemSelected and onNothing selected
-    @Override
-    public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id) {
-        Toast.makeText(getApplicationContext(), units[position], Toast.LENGTH_LONG).show();
-        choice = spin.getSelectedItem().toString();
-
-        b1.setText("");
-        b2.setText("");
-        b3.setText("");
-        b4.setText("");
-        b5.setText("");
-        t2.setText("");
-        t3.setText("");
-        t4.setText("");
-
-        if (choice.equals("Kgs")) {
-            h1.setText(choice);
-            h2.setText("Bundles");
-            h3.setText("Rods");
-        }
-        if (choice.equals("Rods")) {
-            h1.setText(choice);
-            h2.setText("Bundles");
-            h3.setText("Kgs");
-
-        }
-        if (choice.equals("Bundles")) {
-            h1.setText(choice);
-            h2.setText("Kgs");
-            h3.setText("Rods");
-
-
-        }
-
-
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> arg0) {
-        // TODO Auto-generated method stub
-    }
-
-    @Override
-    public void onContentChanged() {
-        super.onContentChanged();
-    }
-
 }
+
+
